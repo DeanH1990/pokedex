ce1ff0b8add02d7c7f79fb67170e6ae52ece5847
DeanH1990 <dhasley1990@gmail.com>
2023-01-12 14:45:50 +0000

    remove console log from api call

--- src/pokeApi.js
+++ src/pokeApi.js
@@ -9,6 +9,5 @@ export const getAllPokemon = async () => {
 export const getPokemonById = async (pokemonId) => {
     const onePokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
 
-    console.log(onePokemon, 'in the api')
     return onePokemon.data;
 }
