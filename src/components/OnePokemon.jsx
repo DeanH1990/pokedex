e378ed2485a41e4b45edfc8ff12d19b6ed865a71
DeanH1990 <dhasley1990@gmail.com>
2023-01-12 14:45:20 +0000

    add functionality to move between each pokemon

--- src/components/OnePokemon.jsx
+++ src/components/OnePokemon.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { getPokemonById } from "../pokeApi";
 
 const OnePokemon = () => {
@@ -7,6 +7,32 @@ const OnePokemon = () => {
     
     const [onePoke, setOnePoke] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+
+    const next = async () => {
+        try {
+            setIsLoading(true);
+            let nextId = parseInt(pokemonId) + 1;
+            const newPoke = await getPokemonById(nextId);
+            setOnePoke(newPoke);
+            setIsLoading(false);
+        } catch (error) {
+            console.log(error);
+            setIsLoading(false);
+        }
+    }
+
+    const back = async () => {
+        try {
+            setIsLoading(true);
+            let nextId = parseInt(pokemonId) - 1;
+            const newPoke = await getPokemonById(nextId);
+            setOnePoke(newPoke);
+            setIsLoading(false);
+        } catch (error) {
+            console.log(error);
+            setIsLoading(false);
+        }
+    }
 
     const loadOne = async () => {
         try {
@@ -26,23 +52,35 @@ const OnePokemon = () => {
     }, [pokemonId])
 
     return isLoading ? <>Loading...</> : (
-        <section className="individual-pokemon">
-            <div className="one-poke-header">
-                <h3>{onePoke.name.charAt(0).toUpperCase() + onePoke.name.slice(1)}</h3>
-                <img className="poke-img" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`} alt={onePoke.name}/>
-            </div>
-            <div className="one-poke-info">
-                <div className="types-and-moves">
-                    <h4 className="types">Types: {onePoke.types.map(({type}) => <p key={type.name}>{type.name}</p>)}</h4>
-                    <h4>No. of moves: {onePoke.moves.length}</h4>
+        <div className="whole-view">
+            {parseInt(pokemonId) > 1 ?
+            <Link to={`/pokemon/${parseInt(pokemonId) - 1}`}>
+                <button onClick={back}>Back</button>
+            </Link>
+            : null}
+            <section className="individual-pokemon">
+                <div className="one-poke-header">
+                    <h3>{onePoke.name.charAt(0).toUpperCase() + onePoke.name.slice(1)}</h3>
+                    <img className="poke-img" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`} alt={onePoke.name}/>
                 </div>
-                <div className="stats">
-                    <h4>Stats: {onePoke.stats.map(({stat, base_stat}) => {
-                        return <p key={stat.name}>{stat.name} {base_stat} </p>
-                    })}</h4>
+                <div className="one-poke-info">
+                    <div className="types-and-moves">
+                        <h4 className="types">Types: {onePoke.types.map(({type}) => <p key={type.name}>{type.name}</p>)}</h4>
+                        <h4>No. of moves: {onePoke.moves.length}</h4>
+                    </div>
+                    <div className="stats">
+                        <h4>Stats: {onePoke.stats.map(({stat, base_stat}) => {
+                            return <p key={stat.name}>{stat.name} {base_stat} </p>
+                        })}</h4>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+            {parseInt(pokemonId) < 151 ?
+            <Link to={`/pokemon/${parseInt(pokemonId) + 1}`}>
+                <button onClick={next}>Next</button>
+            </Link>
+            : null}
+        </div>
     )
 
 }
